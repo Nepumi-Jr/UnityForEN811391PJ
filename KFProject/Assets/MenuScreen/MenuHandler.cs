6b1f1f8fa766c7fe72640e44bec98ef5aa71bfe2
Nepumi-Jr <peerator@kkumail.com>
2021-03-18 21:11:06 +0700

    ✨Example Screen

---
+++ KFProject/Assets/MenuScreen/MenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+
+
+    public GameObject MenuObject;
+    public GameObject GameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doSetScreen("menu");
+    }
+
+    public void doSetScreen(string screen)
+    {
+        MenuObject.SetActive(screen == "menu");
+        GameObject.SetActive(screen == "game");
+    }
+}
